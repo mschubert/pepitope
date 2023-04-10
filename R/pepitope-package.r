3b173873b6f8ae327eb653c11efc6e4f422b2171
mschubert <mschu.dev@gmail.com>
2023-04-10 16:55:25 +0200

    address some BiocCheck errors

--- R/pepitope-package.r
+++ R/pepitope-package.r
@@ -1,4 +1,9 @@
-#' Pepitope package documentation
+#' Pepitope: peptide epitopes from reference genome and variant (VCF) file
 #'
-#' details go here
+#' Given a reference genome and VCF file, this package will provide
+#' the upstream/downstream peptide context of a variant. It will generate a
+#' summary report for protein-coding variants including the reference and
+#' mutated allele, read coverage, amino acid sequence, and other information.
+#' It can also be used to remove restriction sites from cDNA, alongside other
+#' helper functions.
 NULL
