1356743100b811717bf6dd50d4f930c14892dd3f
mschubertv <mschu.dev@gmail.com>
2023-11-08 09:38:25 +0000

    only run annot once (expensive)

--- tests/testthat/test-1-annot.r
+++ tests/testthat/test-1-annot.r
@@ -1,10 +1,5 @@
 context("annotation")
 
-ens106 = AnnotationHub::AnnotationHub()[["AH100643"]]
-asm = BSgenome.Hsapiens.NCBI.GRCh38::BSgenome.Hsapiens.NCBI.GRCh38
-tx = transcripts(ens106, filter=SeqNameFilter("1"))
-tx_coding = names(tx)[tx$tx_biotype=="protein_coding"]
-
 make_vr = function(chr, starts, ref, alt) {
     ends = starts + nchar(ref) - 1
     VariantAnnotation::VRanges(seqnames=chr, ranges=IRanges(starts, ends), ref=ref, alt=alt)
@@ -18,20 +13,33 @@ replaceProt = function(what, where, to) {
     replaceAt(what, as(unname(where), "IRangesList"), as(to, "AAStringSetList"))
 }
 
+ens106 = AnnotationHub::AnnotationHub()[["AH100643"]]
+asm = BSgenome.Hsapiens.NCBI.GRCh38::BSgenome.Hsapiens.NCBI.GRCh38
+tx = transcripts(ens106, filter=SeqNameFilter("1"))
+tx_coding = names(tx)[tx$tx_biotype=="protein_coding"]
+
+vr = unlist(GRangesList(
+    SYN = make_vr("1", c(5969221, 10647824, 18885617), "G", "A"),
+    SNP = make_vr("1", c(1285573, 3763346, 43406323), c("C", "G", "T"), c("T", "A", "C")),
+    STOP = make_vr("1", c(17119295, 39284153, 43312467), c("C", "A", "C"), c("T", "T", "A")),
+    INS = make_vr("1", c(33010831, 21176231, 149509502), c("A", "T", "A"), c("AGGATGT", "TGCC", "AGAAGAC")),
+    DEL = make_vr("1", c(1708855, 51840391, 201386873), c("TTCCTCCTCC", "ATCT", "CCCA"), c("T", "A", "C")),
+    FS = make_vr("1", c(153810271, 33013400, 159062696), c("TG", "G", "GT"), c("T", "GATGTC", "G"))
+))
+ann = suppressWarnings(annotate_coding(vr, ens106, asm, tx_coding))
+
 test_that("synonymous", {
-    vr = make_vr("1", c(5969221, 10647824, 18885617), "G", "A")
+    syn = ann[names(ann) == "SYN"]
+    expect_equal(unique(syn$CONSEQUENCE), "synonymous")
+    expect_true(all(syn$ref_prot == syn$alt_prot))
+    expect_true(all(syn$ref_nuc != syn$alt_nuc))
+    expect_equal(nchar(syn$ref_nuc), nchar(syn$alt_nuc))
+    expect_equal(syn$alt_nuc, replaceDNA(syn$ref_nuc, syn$CDSLOC, syn$varAllele))
+    expect_equal(syn$REFAA, syn$VARAA)
+    expect_true(all(syn$silent_start == 1))
+    expect_true(all(syn$silent_end == 0))
 
-    ann = annotate_coding(vr, ens106, asm, tx_coding)
-    expect_equal(unique(ann$CONSEQUENCE), "synonymous")
-    expect_true(all(ann$ref_prot == ann$alt_prot))
-    expect_true(all(ann$ref_nuc != ann$alt_nuc))
-    expect_equal(nchar(ann$ref_nuc), nchar(ann$alt_nuc))
-    expect_equal(ann$alt_nuc, replaceDNA(ann$ref_nuc, ann$CDSLOC, ann$varAllele))
-    expect_equal(ann$REFAA, ann$VARAA)
-    expect_true(all(ann$silent_start == 1))
-    expect_true(all(ann$silent_end == 0))
-
-    ctx = subset_context(ann, ctx_codons=15)
+    ctx = subset_context(syn, ctx_codons=15)
     expect_true(all(ctx$ref_prot == ctx$alt_prot))
     expect_true(all(ctx$ref_nuc != ctx$alt_nuc))
     expect_true(all(nchar(ctx$ref_nuc) == 90))
@@ -43,23 +51,20 @@ test_that("synonymous", {
 })
 
 test_that("SNPs", {
-    vr = make_vr("1", c(1285573, 3763346, 43406323),
-                 c("C", "G", "T"), c("T", "A", "C"))
+    snp = ann[names(ann) == "SNP"]
+    expect_true(all(snp$CONSEQUENCE == "nonsynonymous"))
+    expect_true(all(snp$ref_prot != snp$alt_prot))
+    expect_true(all(snp$ref_nuc != snp$alt_nuc))
+    expect_equal(nchar(snp$ref_nuc), nchar(snp$alt_nuc))
+    expect_equal(snp[strand(snp) == "+"]$alt, as.character(snp[strand(snp) == "+"]$varAllele))
+    expect_equal(snp[strand(snp) == "-"]$alt, as.character(Biostrings::complement(snp[strand(snp) == "-"]$varAllele)))
+    expect_true(all(snp$alt_nuc == replaceDNA(snp$ref_nuc, snp$CDSLOC, snp$varAllele)))
+    expect_true(all(snp$REFAA != snp$VARAA))
+    expect_true(all(snp$alt_prot == replaceProt(snp$ref_prot, snp$PROTEINLOC, snp$VARAA)))
+    expect_true(all(snp$silent_start == 0))
+    expect_true(all(snp$silent_end == 0))
 
-    ann = annotate_coding(vr, ens106, asm, tx_coding)
-    expect_true(all(ann$CONSEQUENCE == "nonsynonymous"))
-    expect_true(all(ann$ref_prot != ann$alt_prot))
-    expect_true(all(ann$ref_nuc != ann$alt_nuc))
-    expect_equal(nchar(ann$ref_nuc), nchar(ann$alt_nuc))
-    expect_equal(ann[strand(ann) == "+"]$alt, as.character(ann[strand(ann) == "+"]$varAllele))
-    expect_equal(ann[strand(ann) == "-"]$alt, as.character(Biostrings::complement(ann[strand(ann) == "-"]$varAllele)))
-    expect_equal(ann$alt_nuc, replaceDNA(ann$ref_nuc, ann$CDSLOC, ann$varAllele))
-    expect_true(all(ann$REFAA != ann$VARAA))
-    expect_equal(ann$alt_prot, replaceProt(ann$ref_prot, ann$PROTEINLOC, ann$VARAA))
-    expect_true(all(ann$silent_start == 0))
-    expect_true(all(ann$silent_end == 0))
-
-    ctx = subset_context(ann, ctx_codons=15)
+    ctx = subset_context(snp, ctx_codons=15)
     expect_true(all(ctx$ref_prot != ctx$alt_prot))
     expect_true(all(ctx$ref_nuc != ctx$alt_nuc))
     expect_true(all(nchar(ctx$ref_nuc) == 93))
@@ -71,15 +76,12 @@ test_that("SNPs", {
 })
 
 test_that("nonsense", {
-    vr = make_vr("1", c(17119295, 39284153, 43312467),
-                 c("C", "A", "C"), c("T", "T", "A"))
+    stop = ann[names(ann) == "STOP"]
+    expect_true(all(stop$CONSEQUENCE == "nonsense"))
+    expect_true(all(as.character(stop$VARAA) == "*"))
+    expect_true(all(nchar(stop$alt_prot) < nchar(stop$ref_prot)))
 
-    ann = annotate_coding(vr, ens106, asm, tx_coding)
-    expect_true(all(ann$CONSEQUENCE == "nonsense"))
-    expect_true(all(as.character(ann$VARAA) == "*"))
-    expect_true(all(nchar(ann$alt_prot) < nchar(ann$ref_prot)))
-
-    ctx = subset_context(ann, ctx_codons=15)
+    ctx = subset_context(stop, ctx_codons=15)
     expect_true(all(nchar(ctx$ref_nuc) == 93))
     expect_true(all(nchar(ctx$alt_nuc) == 45))
     expect_true(all(nchar(ctx$ref_prot) == 31))
@@ -88,39 +90,30 @@ test_that("nonsense", {
 })
 
 test_that("in-frame insertion", {
-    vr = make_vr("1", c(33010831, 21176231, 149509502),
-                 c("A", "T", "A"), c("AGGATGT", "TGCC", "AGAAGAC"))
+    ins = ann[names(ann) == "INS"]
+    expect_true(all(ins$CONSEQUENCE == "insertion"))
+    expect_true(all(nchar(ins$alt_prot) > nchar(ins$ref_prot)))
 
-    ann = annotate_coding(vr, ens106, asm, tx_coding)
-    expect_true(all(ann$CONSEQUENCE == "insertion"))
-    expect_true(all(nchar(ann$alt_prot) > nchar(ann$ref_prot)))
-
-    ctx = subset_context(ann, ctx_codons=15)
+    ctx = subset_context(ins, ctx_codons=15)
     expect_true(all(nchar(ctx$alt_prot) > nchar(ctx$ref_prot)))
     expect_true(all(ctx$REFAA == "*" | ctx$silent_start > 0))
 })
 
 test_that("in-frame deletion", {
-    vr = make_vr("1", c(1708855, 51840391, 201386873),
-                 c("TTCCTCCTCC", "ATCT", "CCCA"), c("T", "A", "C"))
+    del = ann[names(ann) == "DEL"]
+    expect_true(all(del$CONSEQUENCE == "deletion"))
+    expect_true(all(nchar(del$alt_prot) < nchar(del$ref_prot)))
 
-    ann = annotate_coding(vr, ens106, asm, tx_coding)
-    expect_true(all(ann$CONSEQUENCE == "deletion"))
-    expect_true(all(nchar(ann$alt_prot) < nchar(ann$ref_prot)))
-
-    ctx = subset_context(ann, ctx_codons=15)
+    ctx = subset_context(del, ctx_codons=15)
     expect_equal(nchar(ctx$ref_nuc), 90+width(vr[ctx$QUERYID])-1)
     expect_true(all(nchar(ctx$alt_nuc) == 90))
 })
 
 test_that("frameshift", {
-    vr = make_vr("1", c(153810271, 33013400, 159062696),
-                 c("TG", "G", "GT"), c("T", "GATGTC", "G"))
+    fs = ann[names(ann) == "FS"]
+    expect_true(all(fs$CONSEQUENCE == "frameshift"))
 
-    ann = annotate_coding(vr, ens106, asm, tx_coding)
-    expect_true(all(ann$CONSEQUENCE == "frameshift"))
-
-    ctx = subset_context(ann, ctx_codons=15)
+    ctx = subset_context(fs, ctx_codons=15)
     expect_true(all(nchar(ctx$ref_nuc) %% 3 == 0))
     expect_true(all(nchar(ctx$alt_nuc) %% 3 == 0))
     expect_true(all(nchar(ctx$alt_nuc) == 93))
