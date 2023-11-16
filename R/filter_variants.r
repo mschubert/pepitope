2625743b0bb9c2a02dda42d0606dd48de4de1983
mschubertv <mschu.dev@gmail.com>
2023-11-16 14:48:10 +0000

    raise error if all filter values are NA

--- R/filter_variants.r
+++ R/filter_variants.r
@@ -9,14 +9,25 @@
 filter_variants = function(vr, min_cov=2, min_af=0.05, pass=TRUE) {
     if (!is.null(pass)) {
         passes = apply(softFilterMatrix(vr), 1, all)
-        vr = vr[!is.na(passes) & passes == pass]
+        NAs = is.na(passes)
+        if (all(NAs))
+            stop("Can not apply filter [pass=TRUE] because all values are NA")
+        vr = vr[!NAs & passes == pass]
     }
 
-    if (!is.null(min_cov))
-        vr = vr[!is.na(altDepth(vr)) & altDepth(vr) >= min_cov]
+    if (!is.null(min_cov)) {
+        NAs = is.na(altDepth(vr))
+        if (all(NAs))
+            stop("Can not apply filter [altDepth>=min_cov] because all values are NA")
+        vr = vr[!NAs & altDepth(vr) >= min_cov]
+    }
 
-    if (!is.null(min_af))
-        vr = vr[!is.na(vr$AF) & vr$AF >= min_af]
+    if (!is.null(min_af)) {
+        NAs = is.na(vr$AF)
+        if (all(NAs))
+            stop("Can not apply filter [AF>=min_af] because all values are NA")
+        vr = vr[!NAs & vr$AF >= min_af]
+    }
 
     vr
 }
