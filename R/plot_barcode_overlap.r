7fb5475dbe43766b13b5639d1ae19fca5491f0cd
Michael Schubert <mschu.dev@gmail.com>
2025-07-17 14:20:42 +0200

    annotate invalid barcodes

--- R/plot_barcode_overlap.r
+++ R/plot_barcode_overlap.r
@@ -12,8 +12,16 @@ plot_barcode_overlap = function(all_constructs, valid_barcodes) {
             mutate(overlap = factor(n())) |>
         ungroup()
 
-    if (!missing(valid_barcodes))
+    footer = list()
+    if (!missing(valid_barcodes)) {
+        n_invalid = length(setdiff(dset$barcode, valid_barcodes))
         dset = left_join(data.frame(barcode = valid_barcodes), dset)
+        if (n_invalid == 0) {
+            footer = list(labs(caption = "All barcodes valid"))
+        } else {
+            footer = list(labs(caption = paste(n_invalid, "invalid barcodes dropped")))
+        }
+    }
 
     dset2 = dset |>
         mutate(barcode = as.integer(factor(barcode, levels=unique(barcode)))) |>
@@ -22,5 +30,6 @@ plot_barcode_overlap = function(all_constructs, valid_barcodes) {
     ggplot(dset2, aes(x=barcode, y=bc_type)) +
         geom_tile(aes(fill=overlap)) +
         scale_fill_brewer(palette="Set1") +
-        ggtitle("Barcode overlap")
+        ggtitle("Barcode overlap") +
+        footer
 }
