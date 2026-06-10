a4e18f82d02796d31763b096e5139c60ba235ec7
Michael Schubert <mschu.dev@gmail.com>
2026-06-10 18:30:11 +0000

    make p cutoff configurable

--- R/plot_screen.r
+++ R/plot_screen.r
@@ -6,6 +6,7 @@
 #' @param labs    Whether to label genes in less dense areas
 #' @param min_reads  Minimum number of reads to show as points (default: 10)
 #' @param cap_fc  Maximum amount of fold-change to limit values to
+#' @param p_sig    Maximum adjusted p-value to consider significant (default: 0.05)
 #' @return  A `ggplot2` object of the differential expression results
 #'
 #' @examples
@@ -17,9 +18,9 @@
 #' }
 #'
 #' @export
-plot_screen = function(res, sample=NULL, links=TRUE, labs=TRUE, min_reads=10, cap_fc=8) {
+plot_screen = function(res, sample=NULL, links=TRUE, labs=TRUE, min_reads=10, cap_fc=8, p_sig=0.05) {
     res$log2FoldChange = sign(res$log2FoldChange) * pmin(abs(res$log2FoldChange), cap_fc)
-    lab = res |> filter(padj<0.1) |> group_by(gene_name) |>
+    lab = res |> filter(padj<p_sig) |> group_by(gene_name) |>
         filter(n_distinct(pep_type)==1 | is.na(pep_type) | pep_type=="alt")
 
     p = ggplot(res, aes(x=baseMean, y=log2FoldChange)) +
